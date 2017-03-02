8c6d89ca8d331c7ccf4ef0188d87e0b0486c15b8
Huseyin <huseyinorkunelmas@gmail.com>
2017-03-02 10:07:51 +0300

    first commit

---
+++ datamuse.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 11:19:49 2017
+
+@author: huseyin
+"""
+
+import requests
+import json
+"""
+
+rel_[code]: code parameter should be a list
+jja 	Popular nouns modified by the given adjective, per Google Books Ngrams 	gradual → increase
+jjb 	Popular adjectives used to modify the given noun, per Google Books Ngrams 	beach → sandy
+syn 	Synonyms (words contained within the same WordNet synset) 	ocean → sea
+trg 	"Triggers" (words that are statistically associated with the query word in the same piece of text.) 	cow → milking
+ant 	Antonyms (per WordNet) 	late → early
+spc 	"Kind of" (direct hypernyms, per WordNet) 	gondola → boat
+gen 	"More general than" (direct hyponyms, per WordNet) 	boat → gondola
+com 	"Comprises" (direct holonyms, per WordNet) 	car → accelerator
+par 	"Part of" (direct meronyms, per WordNet) 	trunk → tree
+bga 	Frequent followers (w′ such that P(w′|w) ≥ 0.001, per Google Books Ngrams) 	wreak → havoc
+bgb 	Frequent predecessors (w′ such that P(w|w′) ≥ 0.001, per Google Books Ngrams) 	havoc → wreak
+rhy 	Rhymes ("perfect" rhymes, per RhymeZone) 	spade → aid
+nry 	Approximate rhymes (per RhymeZone) 	forest → chorus
+hom 	Homophones (sound-alike words) 	course → coarse
+cns 	Consonant match 	sample → simple"""
+
+
+"""parameters
+ml(means-like): require that the results have a meaning related to this string value, which can be any word or sequence of words.
+
+sl(sounds-like):require that the results are pronounced similarly to this string of characters. 
+(If the string of characters doesn't have a known pronunciation, the system will make its best 
+guess using a text-to-phonemes algorithm.) 
+
+sp(spelled-like):  A pattern can include any combination of alphanumeric characters, spaces, 
+and two reserved characters that represent placeholders — * (which matches any number of characters) 
+and ? (which matches exactly one character). 
+    
+code: dictionary({jja : word ...}), require that the results, when paired with the word in this parameter, are in a predefined lexical relation indicated by [code].
+Any number of these parameters may be specified any number of times.(theese code are explained above)
+
+v(vocabulary): Identifier for the vocabulary to use
+
+topics (Topic words): An optional hint to the system about the theme of the document being written
+
+max: Maximum number of results to return
+md 	Metadata flags: A list of single-letter codes (no delimiter) requesting
+that extra lexical knowledge be included with the results.
+"""
+import re
+def serch_datamuse_wordenp(ml ,sl=None, sp=None, code=None ,max_res= 100, qe=None):#did not understand qe
+  
+
+        
+    md = '&md=fpd' #hardwired
+    ml = re.sub(" ","+",ml)
+    req_base = "https://api.datamuse.com/words?max_res=" +str(max_res)+'&'
+    req_base= req_base + "ml="+ml
+    
+    if sl is not None:
+        req_base = req_base + '&sl=' +sl
+    
+    if sp is not None:
+        req_base = req_base + '&sp=' + sp
+    
+    code_string = ''
+    for key,item in code.items():
+        code_string = code_string + "&rel_"+ key +"=" + item
+    
+    
+    req_addr = req_base + code_string+md
+    print(req_addr)
+#r = requests.get(req_addr)
+
+
+def serch_datamuse_sug(s, max_res):
+    req_base = "https://api.datamuse.com/sug?"
+   
+    req_addr = req_base + 's=' + s+'&max='+str(max_res)
+    r = requests.get(req_addr)
+
+    print(r.text)
+code = {'jja':"yellow", 'jjb':'test'}
+#serch_datamuse_wordenp("ringing in the ears",sp = 'a*' ,code = code)
+
+serch_datamuse_sug(s = 'rawand', max_res = 10)
+
+
+
+
+
+
+
+
+
+
